a34ba57d53799574dc2b26df822d8293c30b661b
Benjamin Ruthard <benjamin.ruthard@Benjamin-Ruthard.local>
2018-09-17 13:54:45 -0400

    MathExample

--- MathExample.py
+++ MathExample.py
@@ -1,3 +1,9 @@
-x=1+2
+x = 1 + 2
 print (x)
+#These are comments in Java. When you program
+#and I look at your work I always check your comments.
+ #This creates a variable and called x and
 
+
+x = x + 1
+print(x)
